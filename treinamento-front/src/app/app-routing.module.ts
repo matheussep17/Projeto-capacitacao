4b23be91c5c0d164169232b789bc38955303d9f7
matheus <matheussep17@gmail.com>
2019-05-18 13:55:34 -0300

    ALteraçao de rotas

--- treinamento-front/src/app/app-routing.module.ts
+++ treinamento-front/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', loadChildren: './domain/dashboard/dashboard.module#DashboardModule' },
   { path: 'dashboard', loadChildren: './domain/dashboard/dashboard.module#DashboardModule' },
   { path: 'pais', loadChildren: './domain/pais/pais.module#PaisModule' },
   { path: 'garagem', loadChildren: './domain/garagem/garagem.module#GaragemModule' },
